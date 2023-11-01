c1946e965845100385cb48aa3c543fafe9faa2f7
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 06:27:43 +0100

    add

--- components/UploadButton.jsx
+++ components/UploadButton.jsx
@@ -1,19 +1,17 @@
 "use client"
 
 import { CldUploadWidget } from 'next-cloudinary';
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 function UploadButton() {
   const uploadPreset = 'my-upload-preset';
 
-  const handleOnClick = async () => {
+  async function handleOnClick() {
     // Get the public ID of the uploaded file
-    const publicId = await CldUploadWidget({
-      uploadPreset,
-    }).open();
+    const publicId = await CldUploadWidget({ uploadPreset }).open();
 
     // Do something with the public ID
-  };
+  }
 
   return (
     <Button onClick={handleOnClick}>Upload</Button>
