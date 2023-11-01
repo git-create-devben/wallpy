b5847f8ae8d956bafbb046a32bcb2a7296029c73
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 05:40:41 +0100

    adjust

--- components/UploadButton.jsx
+++ components/UploadButton.jsx
@@ -2,24 +2,24 @@
 
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { CldUploadWidget } from 'next-cloudinary';
+import { CldUploadWidget } from "next-cloudinary";
 
 const UploadButton = () => {
-    <div>
-      <CldUploadWidget uploadPreset="<Upload Preset>">
-      {({ open }) => {
-        function handleOnClick(e) {
-          e.preventDefault();
-          open();
-        };
-        return (
+  <CldUploadWidget uploadPreset="<Upload Preset>">
+    {({ open }) => {
+      function handleOnClick(e) {
+        e.preventDefault();
+        open();
+      }
+      return (
+        <>
           <Button className="button" onClick={handleOnClick}>
             Upload an Image
           </Button>
+        </>
       );
     }}
-    </CldUploadWidget>
-    </div>
+  </CldUploadWidget>;
 };
 
 export default UploadButton;
