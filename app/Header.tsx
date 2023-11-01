128adf16648ba0e016d07cd83b45f51267cec303
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 05:19:55 +0100

    add button

--- app/Header.tsx
+++ app/Header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
         />
       </div>
       <div className=" flex gap-4">
-        <Button></Button>
+        <Button> Add portifolio</Button>
       <ModeToggle/>
       <UserButton afterSignOutUrl="/" />
       </div>
