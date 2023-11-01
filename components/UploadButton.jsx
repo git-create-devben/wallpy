58917c987dedc837a46ff36f00d54cff86d195ab
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 05:47:06 +0100

    change

--- components/UploadButton.jsx
+++ components/UploadButton.jsx
@@ -2,22 +2,26 @@
 
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { CldUploadWidget } from "next-cloudinary";
+import { CldUploadWidget } from 'next-cloudinary';
 
-const UploadButton = () => {
-  <CldUploadWidget uploadPreset="<Upload Preset>">
-    {({ open }) => {
-      function handleOnClick(e) {
-        e.preventDefault();
-        open();
-      }
-      return (
-          <Button className="button" onClick={handleOnClick}>
-            Upload an Image
-          </Button>
-      );
-    }}
-  </CldUploadWidget>;
-};
+function UploadButton() {
+  const openUploadWidget = () => {
+    // Open the Cloudinary upload widget
+  };
+
+  const UploadButtonContent = () => {
+    return (
+      <CldUploadWidget uploadPreset="<Upload Preset>" onOpen={openUploadWidget}>
+        <Button className="button" onClick={openUploadWidget}>
+          Upload an Image
+        </Button>
+      </CldUploadWidget>
+    );
+  };
+
+  return (
+    <UploadButtonContent />
+  );
+}
 
 export default UploadButton;
