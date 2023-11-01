2a610b0033037ef68480bd20ac2f3e50b98dae41
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 06:43:28 +0100

    change

--- components/UploadButton.jsx
+++ components/UploadButton.jsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { CldUploadWidget } from 'next-cloudinary';
+import { CldUploadWidget } from "next-cloudinary";
 import { Button } from "@/components/ui/button";
 
 function UploadButton() {
-  const uploadPreset = 'my-upload-preset';
+  const uploadPreset = "my-upload-preset";
 
   async function handleOnClick() {
     // Get the public ID of the uploaded file
@@ -15,8 +15,24 @@ function UploadButton() {
 
   return (
     <>
+      <div className="dropdown dropdown-left dropdown-end">
+        <label tabIndex={0} className="btn m-1">
+          Click
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+        </ul>
+      </div>
+      <Button onClick={handleOnClick}>Upload</Button>
     </>
-    <Button onClick={handleOnClick}>Upload</Button>
   );
 }
 
