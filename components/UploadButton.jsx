4dbbef062a747cd0149ee2861211c18f84b22db8
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 06:44:50 +0100

    remve

--- components/UploadButton.jsx
+++ components/UploadButton.jsx
@@ -15,22 +15,7 @@ function UploadButton() {
 
   return (
     <>
-      <div className="dropdown dropdown-left dropdown-end">
-        <label tabIndex={0} className="btn m-1">
-          Click
-        </label>
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 2</a>
-          </li>
-        </ul>
-      </div>
+    
       <Button onClick={handleOnClick}>Upload</Button>
     </>
   );
