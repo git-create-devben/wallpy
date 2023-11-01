fd9bfd0d1d3b91b39173c36b35a2cff452ccdd06
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 05:19:08 +0100

    update

--- app/Header.tsx
+++ app/Header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
         />
       </div>
       <div className=" flex gap-4">
-        
+        <Button></Button>
       <ModeToggle/>
       <UserButton afterSignOutUrl="/" />
       </div>
