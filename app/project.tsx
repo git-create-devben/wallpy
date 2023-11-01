05344460da3534b8287456f8020fac78a2ef7178
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 07:22:55 +0100

    add

--- app/project.tsx
+++ app/project.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { PortfolioItem } from "r";
+import { useRouter } from "next/router";
+import Image from 'next/image';
 
 const project = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
+  const { fileUrl } = router.query;
+
+  const portfolioItem: PortfolioItem = {
+    imageUrl: fileUrl,
+    link: fileUrl,
+  };
+
   return (
-    <div>project</div>
-  )
+    <div>
+      <Image src={portfolioItem.imageUrl} alt="" />
+      <a href={portfolioItem.link}>Visit portfolio</a>
+    </div>
+  );
 }
 
 export default project
--- components/UploadButton.jsx
+++ components/UploadButton.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-const UploadButton = () => {
+type PortfolioItem = {
+  imageUrl: string;
+  link: string;
+};
+
+const PortfolioUploader: React.FC = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [link, setLink] = useState<string>("");
+
+  const router = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(e.target.files?.[0]);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Upload the file
+    const response = await fetch("/api/upload", {
+      method: "POST",
+      body: new FormData().append("file", file),
+    });
+
+    const fileUrl = await response.json();
+
+    // Set the image URL and link
+    setImageUrl(fileUrl);
+    setLink(link);
+
+    // Navigate to the portfolio item page
+    router.push(`/portfolio/${fileUrl}`);
+  };
+
   return (
-    <div>UploadButton</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="Link to your portfolio"
+        value={link}
+        onChange={(e) => setLink(e.target.value)}
+      />
+      <button type="submit">Upload</button>
+    </form>
+  );
+};
 
-export default UploadButton
+export default PortfolioUploader;
