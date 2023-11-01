cc1ecf91da57d31696936854d71fae5e7db389a6
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 05:13:32 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        loader: "custom",
-        // loaderFile: './node_modules/@uploadcare/nextjs-loader/build/loader.js'
-        // loaderFile: 'C:/Users/user/wallpy/node_modules/@uploadcare/nextjs-loader/build/loader.js',
-      }
+    // images: {
+    //     loader: "custom",
+    //   }
 }
 
 module.exports = nextConfig
