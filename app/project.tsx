e25802e241f9f6a5b89f23551c2ee4358fd12be9
git-create-devben <DevBen.tech@outlook.com>
2023-11-01 05:16:10 +0100

    update

--- app/project.tsx
+++ app/project.tsx
@@ -1,29 +1,9 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react'
 
 const project = () => {
   return (
-    <main className="p-4 ">
-      <div className="card w-96 bg-base-100 shadow-xl image-full">
-        <figure>
-          <Image
-            alt="A test image"
-            src="https://your-domain/image.jpg"
-            width="400"
-            height="300"
-            quality="80"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-};
+    <div>project</div>
+  )
+}
 
-export default project;
+export default project
