967b4364132c23f07b11c82a2d2a0b3012658932
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 06:40:52 +0100

    styles

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className=" flex">
+    <div className=" flex justify-center">
       <SignIn />
     </div>
   );
