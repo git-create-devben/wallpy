5e511002d242225c520a5e714f99596fda5e9c31
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 16:46:38 +0100

    add

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -11,7 +11,13 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { storage } from "../app/firebase";
-import { ref, uploadBytes, listAll, getDownloadURL, ListResult } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+  ListResult,
+} from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
 
 import React from "react";
@@ -25,8 +31,9 @@ export const Uploadbutton = () => {
     if (imageUpload === null) return;
     const imageRef = ref(storage, `images/${imageUpload.name + uuidv4()}`);
     uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {})
-      setImageList((prev) => [...prev,])
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, url]);
+      });
     });
   };
 
@@ -56,11 +63,19 @@ export const Uploadbutton = () => {
             <DialogTitle>Upload a cover pic of your project</DialogTitle>
             <DialogDescription>
               <main className="flex flex-col items-center justify-between p-24">
-                <input type="file" onChange={(e) => setImageUpload(e.target.files?.[0] || null)} />
+                <input
+                  type="file"
+                  onChange={(e) => setImageUpload(e.target.files?.[0] || null)}
+                />
                 <button onClick={uploadImage}>Upload</button>
                 {imageList.map((url, index) => (
                   <div key={index}>
-                    <Image src={url} alt={`image-${index}`} width={200} height={200} />
+                    <Image
+                      src={url}
+                      alt={`image-${index}`}
+                      width={200}
+                      height={200}
+                    />
                   </div>
                 ))}
               </main>
