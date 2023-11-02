96a48f1190a33ba708e0288386ce4c90d2499952
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 06:48:32 +0100

    add

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 const Hero = () => {
   return (
     <section>
+      <Header/>
       <div
         className="hero h-[30rem]"
         style={{
