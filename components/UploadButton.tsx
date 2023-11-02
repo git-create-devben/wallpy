39293811b621bd2430886ce8ba74625d46ec8547
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 07:30:58 +0100

    replace

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -5,18 +5,6 @@ import { UploadButton } from "../utils/uploadthing";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
       {/* Open the modal using document.getElementById('ID').showModal() method */}
       <button
         className="btn"
@@ -33,6 +21,18 @@ export default function Home() {
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
+              <UploadButton
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                  // Do something with the response
+                  console.log("Files: ", res);
+                  alert("Upload Completed");
+                }}
+                onUploadError={(error: Error) => {
+                  // Do something with the error.
+                  alert(`ERROR! ${error.message}`);
+                }}
+              />
               <button className="btn">Close</button>
             </form>
           </div>
