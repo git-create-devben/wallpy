38007819a95f402349db40b6b05dc2d353e92ed3
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 09:57:47 +0100

    style

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -3,10 +3,14 @@ import "./project.css";
 
 const project = () => {
   return (
-    <main className="flex gap-2 justify-center">
-     <div className=" w-[600px] h-[500px] bg-green-600">
-
-     </div>
+    <main className="flex gap-2 p-2">
+      <div className=" w-[500px] h-[400px] bg-green-600"></div>
+      <div className=" w-[500px] h-[400px] bg-green-600"></div>
+      <div className=" w-[500px] h-[400px] bg-green-600"></div>
+      <div className=" w-[500px] h-[400px] bg-green-600"></div>
+      <div className=" w-[500px] h-[400px] bg-green-600"></div>
+      <div className=" w-[500px] h-[400px] bg-green-600"></div>
+      
     </main>
   );
 };
