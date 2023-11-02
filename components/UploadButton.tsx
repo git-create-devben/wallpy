50c89e04e0fa0c8ee2d483fc435bbe93482c5ee7
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 08:11:29 +0100

    add

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -8,12 +8,38 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+ 
+import { UploadButton } from "../utils/uploadthing";
 
 
 import React from 'react'
 
 export const UploadButton = () => {
   return (
-    <div>UploadButton</div>
+    <div>
+      <Dialog>
+  <DialogTrigger>Open</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+      <DialogDescription>
+      <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={(res) => {
+              // Do something with the response
+              console.log("Files: ", res);
+              alert("Upload Completed");
+            }}
+            onUploadError={(error: Error) => {
+              // Do something with the error.
+              alert(`ERROR! ${error.message}`);
+            }}
+          />
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+
+    </div>
   )
 }
