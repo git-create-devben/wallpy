4828d28a326598414ba8b099a140db0c3d7e827f
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 16:04:56 +0100

    add packages

--- app/firebase.ts
+++ app/firebase.ts
@@ -20,4 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const storage = getStorage(app)
+export const storage = getStorage(app)
