d663f7d3e8826340f25ae4ae4cfc1bd564f9f0e8
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 09:26:53 +0100

    typo

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import project from "@/components/project/project";
+import Project from "@/components/project/project";
 
 const Hero = () => {
   return (
