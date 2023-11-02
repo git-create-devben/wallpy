8135fc6955ca217d234d78fd48764dd6ab8894a0
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 06:46:08 +0100

    bug

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className=" flex justify-center items-center w-full h-[100vh]">
-      <SignUp />;
+      <SignUp />
     </div>
   );
 }
