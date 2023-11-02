c68cfbc4ff1ef846b376ab061b01bdb11ea8ba11
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 12:10:23 +0100

    add

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -6,7 +6,7 @@ const project = () => {
   return (
     <main className="flex gap-4 flex-wrap">
       <div className="parent-div w-[450px] h-[400px] bg-green-600 relative rounded-xl">
-        <Image src={} height="40 width="450px" alt="Portfolio background image"/>
+        <Image src={} height="400" width="450" alt="Portfolio background image"/>
         <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 animate-after:w-[70%]">
          {/* I want the content in this div shows */}
         </div>
