8c5bea50a263535fea3c8e13f0f5aee910a7f079
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 17:54:09 +0100

    clearing

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -9,22 +9,17 @@ interface ProjectProps {
 const Project: React.FC<ProjectProps> = ({ imageList }) => {
   return (
     <main className="flex gap-4 flex-wrap">
-      <div className="parent-div w-[450px] h-[400px] bg-green-600 relative">
-         {/* If i over on this parent div the children div we show  */}
-         {imageList.map((url, index) => (
-        <div key={index}>
-          <Image src={url} alt={`image-${index}`} width={200} height={200} />
+      {/* If i over on this parent div the children div we show  */}
+      {imageList.map((url, index) => (
+        <div
+          key={index}
+          className="parent-div w-[450px] h-[400px] bg-green-600 relative"
+        >
+          <Image src={url} alt={`image-${index}`} width={450} height={200} />
+          <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 ">
+          </div>
         </div>
       ))}
-
-        <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 ">
-         {/* I want the content in this div shows */}
-        </div>
-      </div>
-      <div className=" w-[450px] h-[400px] bg-green-600"></div>
-      <div className=" w-[450px] h-[400px] bg-green-600"></div>
-      <div className=" w-[450px] h-[400px] bg-green-600"></div>    
-      
     </main>
   );
 };
