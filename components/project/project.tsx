db65b94612c7279634ffa7983c14d86aac0ce57f
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 13:28:55 +0100

    add

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,29 +1,33 @@
-"use client"
-
 import React from "react";
 import "./project.css";
 import Image from "next/image";
-import { useState } from "react";
-// import { UploadButton } from "../utils/uploadthing";
-// import Uploadbutton from "../UploadButton"
 
+interface ProjectProps {
+  imageURL: string;
+}
 
-
-
-export const Project = () => {
-
+const Project: React.FC<ProjectProps> = ({ imageURL }) => {
   return (
     <main className="flex gap-4 flex-wrap">
       <div className="parent-div w-[450px] h-[400px] bg-green-600 relative rounded-xl">
-        <Image src="" height="400" width="450" alt="Portfolio background image" className="rounded-xl"/>
-        <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 animate-after:w-[70%]">
-         {/* I want the content in this div shows */}
+        {imageURL ? (
+          <Image
+            src={imageURL}
+            height="400"
+            width="450"
+            alt="Portfolio background image"
+            className="rounded-xl"
+          />
+        ) : (
+          <p>No image uploaded yet</p>
+        )}
+        <div className="child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 animate-after:w-[70%]">
+          {/* Add content here */}
         </div>
       </div>
-      <div className=" w-[450px] h-[400px] bg-green-600"></div>
-      <div className=" w-[450px] h-[400px] bg-green-600"></div>
-      <div className=" w-[450px] h-[400px] bg-green-600"></div>    
-      
+      <div className="w-[450px] h-[400px] bg-green-600"></div>
+      <div className="w-[450px] h-[400px] bg-green-600"></div>
+      <div className="w-[450px] h-[400px] bg-green-600"></div>
     </main>
   );
 };
