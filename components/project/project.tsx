ebb16eb6866226e33a9e2265197db90671d72f3a
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 17:56:19 +0100

    clear

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -11,13 +11,9 @@ const Project: React.FC<ProjectProps> = ({ imageList }) => {
     <main className="flex gap-4 flex-wrap">
       {/* If i over on this parent div the children div we show  */}
       {imageList.map((url, index) => (
-        <div
-          key={index}
-          className="parent-div  bg-green-600 relative"
-        >
+        <div key={index} className="parent-div  bg-green-600 relative">
           <Image src={url} alt={`image-${index}`} width={450} height={100} />
-          <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 ">
-          </div>
+          <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 "></div>
         </div>
       ))}
     </main>
