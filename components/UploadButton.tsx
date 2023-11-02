287449a4fb9eca15b12eefe1351a2fe2c8ebc6a0
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 07:29:09 +0100

    add

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,7 +1,7 @@
 "use client";
- 
+
 import { UploadButton } from "../utils/uploadthing";
- 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -17,6 +17,27 @@ export default function Home() {
           alert(`ERROR! ${error.message}`);
         }}
       />
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_5").showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </main>
   );
 }
