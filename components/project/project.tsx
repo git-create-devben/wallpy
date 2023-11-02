c9fd6406289c94dc8b462763603d1d0480f1652c
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 19:03:45 +0100

    modify

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -24,10 +24,10 @@ const Project: React.FC<ProjectProps> = ({ imageList, setLinkProp, githubLink, p
           <Image src={url} alt={`image-${index}`} width={450} height={100} />
           <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 ">
             <ul className="flex justify-between p-2">
-              <Link href={portfolioLink} className=" flex flex-1"> 
+              <Link href={portfolioLink ? portfolioLink : ""} className=" flex flex-1"> 
                <BiLinkExternal/>
               </Link>
-              <Link href={socialLink}> 
+              <Link href={socialLink ? socialLink : ""}> 
                <FaXTwitter/>
               </Link>
               <Link href={githubLink ? githubLink : ""}> 
