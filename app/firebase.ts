a657d8c1c30c91fb6a962967db0453fb591ac380
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 15:46:57 +0100

    add firebase

---
+++ app/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAka9jZ14MJwqJLG7FsJ4Vgcb8h774XTkU",
+  authDomain: "wallpy5.firebaseapp.com",
+  projectId: "wallpy5",
+  storageBucket: "wallpy5.appspot.com",
+  messagingSenderId: "434810454717",
+  appId: "1:434810454717:web:024e77770cc3f6515bedd4",
+  measurementId: "G-NTLXHXNES4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
