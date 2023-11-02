75d6a51f62f9da2427c5bb44a9934eb3acf996af
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 09:43:57 +0100

    change

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -4,16 +4,16 @@ import "./project.css";
 const project = () => {
   return (
     <main>
-<div className="card">
-  <div className="top">
-    <p className="title">
-      TITLE HERE
-    </p>
-  </div>
-  <p className="desc">
-    This is the description of this card.<br/>You can enter anything here.
-  </p>
-</div>
+      <div className="card">
+        <div className="top">
+          <p className="title">TITLE HERE</p>
+        </div>
+        <p className="desc">
+          This is the description of this card.
+          <br />
+          You can enter anything here.
+        </p>
+      </div>
     </main>
   );
 };
