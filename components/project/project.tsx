897ffa274ca80015b0be3d940a0ea00865bf3233
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 13:55:39 +0100

    change

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -3,11 +3,13 @@ import "./project.css";
 import Image from "next/image";
 
 const project = () => {
+
+
+  
   return (
     <main className="flex gap-4 flex-wrap">
       <div className="parent-div w-[450px] h-[400px] bg-green-600 relative rounded-xl">
         <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 animate-after:w-[70%]">
-         {/* I want the content in this div shows */}
         </div>
       </div>      
     </main>
