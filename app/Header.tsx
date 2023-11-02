f05651a64bb6c8dfdd63f782903b907045bcaa3c
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 06:49:19 +0100

    change

--- app/Header.tsx
+++ app/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className=" flex justify-between p-2">
       <div className=" flex items-center text-center gap-6 w-full mb-2">
-        <h2 className=" text-6xl font-bold mb-2">
+        <h2 className=" text-4xl font-bold mb-2">
           <span className=" text-blue-500">W</span>
           <span className=" text-red-500">a</span>
           <span className=" text-green-500">l</span>
