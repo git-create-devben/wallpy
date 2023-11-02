ea7a1cda846ec9927437ae9a808581dd80734e02
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 07:10:27 +0100

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import { withUt } from "uploadthing/tw";
+export default withUt({
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -74,4 +75,4 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
-}
+});
