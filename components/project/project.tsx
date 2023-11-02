21d68c628c4c5d957da2bbca00ee08726b3e74ee
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 12:19:13 +0100

    add

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -16,7 +16,14 @@ export const Project = () => {
   return (
     <main className="flex gap-4 flex-wrap">
       <div className="parent-div w-[450px] h-[400px] bg-green-600 relative rounded-xl">
-        <Image src="" height="400" width="450" alt="Portfolio background image" className="rounded-xl"/>
+      <ul>
+        {images.map((imager) => (
+          <li key={imager.fileUrl} className="m-2">
+            <Image src={imager.fileUrl} height={200} width={200} alt="upload"/>
+          </li>
+        ))}
+      </ul>
+        {/* <Image src={fileUrl} height="400" width="450" alt="Portfolio background image" className="rounded-xl"/> */}
         <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 animate-after:w-[70%]">
          {/* I want the content in this div shows */}
         </div>
@@ -24,7 +31,13 @@ export const Project = () => {
       <div className=" w-[450px] h-[400px] bg-green-600"></div>
       <div className=" w-[450px] h-[400px] bg-green-600"></div>
       <div className=" w-[450px] h-[400px] bg-green-600"></div>    
-      
+       <ul>
+        {images.map((imager) => (
+          <li key={imager.fileUrl} className="m-2">
+            <Image src={imager.fileUrl} height={200} width={200} alt="upload"/>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
