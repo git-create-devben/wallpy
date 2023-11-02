28de72aac2cdd1a711e5ab9fbed8a7f363955583
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 07:37:05 +0100

    change

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -6,33 +6,28 @@ export default function Home() {
   return (
     <main>
       {/* Open the modal using document.getElementById('ID').showModal() method */}
-      <button
-        className="btn"
-      
-      >
-        open modal
-      </button>
+      <button className="btn">open modal</button>
       <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
           <h3 className="font-bold text-lg">Hello!</h3>
           <p className="py-4">
             Press ESC key or click the button below to close
           </p>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={(res) => {
+              // Do something with the response
+              console.log("Files: ", res);
+              alert("Upload Completed");
+            }}
+            onUploadError={(error: Error) => {
+              // Do something with the error.
+              alert(`ERROR! ${error.message}`);
+            }}
+          />
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <UploadButton
-                endpoint="imageUploader"
-                onClientUploadComplete={(res) => {
-                  // Do something with the response
-                  console.log("Files: ", res);
-                  alert("Upload Completed");
-                }}
-                onUploadError={(error: Error) => {
-                  // Do something with the error.
-                  alert(`ERROR! ${error.message}`);
-                }}
-              />
               <button className="btn">Close</button>
             </form>
           </div>
