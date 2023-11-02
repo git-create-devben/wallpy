e41f784bf72f6ef529a54a8a555e19a65a23ab08
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 20:05:13 +0100

    modify

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ import Hero from "./Hero";
 export default function Home() {
   return (
     <div>
-           <Hero setImageListProp={function (newImageList: string[]): void {
-        throw new Error("Function not implemented.");
-      } } setLinkProp={function (newLink: { github: string; portfolio: string; social: string; }): void {
-        throw new Error("Function not implemented.");
-      } } githubLink={""} portfolioLink={""} socialLink={""}/>
+           <Hero/>
     </div>
   );
 }
