0b440bb89d3f3ab118b6ad3f1d25e23dfbca2c98
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 15:49:36 +0100

    clear

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -17,34 +17,7 @@ import Image from "next/image";
 import React from "react";
 
 export const Uploadbutton = () => {
-  const [images, setImage] = useState<
-    {
-      fileUrl: string;
-      fileKey: string;
-    }[]
-  >([]);
 
-  const head = images.length ? (
-    <>
-      <p>`Upload sucessful 🥳</p>
-      <p className="m-2">{images.length} file</p>
-    </>
-  ) : null;
-
-  const imglist = (
-    <>
-      {/* <p>{head}</p> */}
-      <ul>
-        {images.map((imager) => (
-          <li key={imager.fileUrl} className="m-2">
-            <Link href={imager.fileUrl}>
-            <Image src={imager.fileUrl} height={200} width={200} alt="upload"/>
-            </Link>          
-          </li>
-        ))}
-      </ul>
-    </>
-  );
 
   return (
     <div>
@@ -55,24 +28,7 @@ export const Uploadbutton = () => {
             <DialogTitle>Upload a cover pics of your project</DialogTitle>
             <DialogDescription>
               <main className="flex  flex-col items-center justify-between p-24">
-                <UploadButton
-                  endpoint="imageUploader"
-                  onClientUploadComplete={(res) => {
-                    if (res) {
-                      setImage(res)
-                      const json = JSON.stringify(res)
-                      // Do something with the response
-                      console.log(json);
-                    }
-
-                    // alert("Upload Completed");
-                  }}
-                  onUploadError={(error: Error) => {
-                    // Do something with the error.
-                    alert(`ERROR! ${error.message}`);
-                  }}
-                />
-                {imglist}
+          
               </main>
             </DialogDescription>
           </DialogHeader>
