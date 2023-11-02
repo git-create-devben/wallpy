5a3dbcc525e6f030b6d3ca951e9a066e8868d9f8
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 12:06:12 +0100

    style

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -4,7 +4,7 @@ import "./project.css";
 const project = () => {
   return (
     <main className="flex gap-4 flex-wrap">
-      <div className="parent-div w-[450px] h-[400px] bg-green-600 relative">
+      <div className="parent-div w-[450px] h-[400px] bg-green-600 relative rounded-xl">
          {/* If i over on this parent div the children div we show  */}
         <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 animate-after:w-[70%]">
          {/* I want the content in this div shows */}
