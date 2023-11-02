4d6d2bc050a2a7240d7f80ac7a831d5d95672e08
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 10:00:49 +0100

    styles

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -3,7 +3,7 @@ import "./project.css";
 
 const project = () => {
   return (
-    <main className="flex gap-2 p-2">
+    <main className="flex gap-2 p-2 row-4">
       <div className=" w-[500px] h-[400px] bg-green-600"></div>
       <div className=" w-[500px] h-[400px] bg-green-600"></div>
       <div className=" w-[500px] h-[400px] bg-green-600"></div>
