8a1e4082de66370c4fbd19f6a20de3019fa6d595
git-create-devben <DevBen.tech@outlook.com>
2023-11-02 12:32:50 +0100

    rempve

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import "./project.css";
@@ -8,31 +8,57 @@ import { useState } from "react";
 
 export const Project = () => {
   const [images, setImage] = useState<
-  {
-    fileUrl: string;
-    fileKey: string;
-  }[]
->([]);
+    {
+      fileUrl: string;
+      fileKey: string;
+    }[]
+  >([]);
+
+  const head = images.length ? (
+    <>
+      <p>`Upload sucessful 🥳</p>
+      <p className="m-2">{images.length} file</p>
+    </>
+  ) : null;
+
+  const imglist = (
+    <>
+      {/* <p>{head}</p> */}
+      <ul>
+        {images.map((imager) => (
+          <li key={imager.fileUrl} className="m-2">
+            <Image src={imager.fileUrl} height={200} width={200} alt="upload" />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+
   return (
     <main className="flex gap-4 flex-wrap">
       <div className="parent-div w-[450px] h-[400px] bg-green-600 relative rounded-xl">
         {images.map((imager) => (
           <li key={imager.fileUrl} className="m-2">
-            <Image src="https://utfs.io/f/6a1e2b56-9a89-4035-9f1c-ea1d337a0f5e-cbnelv.jpg" height={400} width={450} alt="upload"/>
+            <Image
+              src="https://utfs.io/f/6a1e2b56-9a89-4035-9f1c-ea1d337a0f5e-cbnelv.jpg"
+              height={400}
+              width={450}
+              alt="upload"
+            />
           </li>
         ))}
         {/* <Image src={fileUrl} height="400" width="450" alt="Portfolio background image" className="rounded-xl"/> */}
         <div className=" child-div absolute bottom-0 right-0 w-[450px] h-[50px] bg-red-500 animate-after:w-[70%]">
-         {/* I want the content in this div shows */}
+          {/* I want the content in this div shows */}
         </div>
       </div>
       <div className=" w-[450px] h-[400px] bg-green-600"></div>
       <div className=" w-[450px] h-[400px] bg-green-600"></div>
-      <div className=" w-[450px] h-[400px] bg-green-600"></div>    
-       <ul>
+      <div className=" w-[450px] h-[400px] bg-green-600"></div>
+      <ul>
         {images.map((imager) => (
           <li key={imager.fileUrl} className="m-2">
-            <Image src={imager.fileUrl} height={200} width={200} alt="upload"/>
+            <Image src={imager.fileUrl} height={200} width={200} alt="upload" />
           </li>
         ))}
       </ul>
