dc8e4d0a1f66f780c1e7495a440baa212f40562d
git-create-devben <DevBen.tech@outlook.com>
2023-11-03 22:52:54 +0100

    update

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import firebaseConfig from '../app/firebase'; // Update the path to your Firebase config file
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 interface Developer {
   name: string;
-  thumbnail: string | Blob;
+  thumbnail: File | null;
   github: string;
   twitter: string;
   portfolioUrl: string;
 }
 
 const UploadButton: React.FC = () => {
-  const supabaseUrl = 'https://vkarozfhfwvtmbjgtmzi.supabase.co'; // Replace with your Supabase URL
-  const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrYXJvemZoZnd2dG1iamd0bXppIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwMzc4MDgsImV4cCI6MjAxNDYxMzgwOH0.Ne6deynwQtKFd6bw6fFCWPK-QSDDv3JmTn6UsoiLxkw" // Replace with your Supabase anon key
-
-  const supabase = createClient(supabaseUrl, supabaseAnonKey);
   const [developer, setDeveloper] = useState<Developer>({
     name: '',
-    thumbnail: '',
+    thumbnail: null,
     github: '',
     twitter: '',
     portfolioUrl: '',
@@ -29,100 +31,34 @@ const UploadButton: React.FC = () => {
     });
   };
 
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-  
-    setIsSubmitting(true);
-  
-    try {
-      // Upload the thumbnail image to Supabase Storage.
-      const storage = supabase.storage;
-      const { data, error } = await storage
-        .from('thumbnail')
-        .upload(`thumbnail/${developer.name}`, developer.thumbnail, {
-          cacheControl: '3600',
-          upsert: false,
-        });
-  
-      if (error) {
-        console.error('Error uploading file:', error);
-      } else {
-        console.log('File uploaded successfully. File info:', data);
-  
-        // Save the developer information to Supabase Database.
-        const databaseRef = supabase.from('developers');
-        await databaseRef.insert({
-          name: developer.name,
-          thumbnailUrl: data?.path, // Adjust as per your data structure
-          github: developer.github,
-          twitter: developer.twitter,
-          portfolioUrl: developer.portfolioUrl,
-        });
-  
-        // Clear the form.
-        setDeveloper({
-          name: '',
-          thumbnail: '',
-          github: '',
-          twitter: '',
-          portfolioUrl: '',
-        });
-  
-        setIsSubmitting(false);
-      }
-    } catch (error) {
-      console.error('Error submitting form:', error);
-      setIsSubmitting(false);
+
+    const storageRef = firebase.storage().ref();
+    if (developer.thumbnail) {
+      const uploadTask = storageRef.child(`thumbnails/${developer.name}`).put(developer.thumbnail);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          // You can handle progress here if needed
+        },
+        (error) => {
+          console.error('Error uploading file:', error);
+        },
+        () => {
+          console.log('File uploaded successfully.');
+          // Handle the rest of your logic here, e.g., saving the developer information to Firebase Firestore.
+        }
+      );
+    } else {
+      console.error('No file chosen.');
     }
   };
-  
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Name"
-        value={developer.name}
-        onChange={handleChange}
-      />
-      <input
-        type="file"
-        name="thumbnail"
-        placeholder="Thumbnail"
-        onChange={(e) => {
-          if (e.target.files) {
-            setDeveloper({
-              ...developer,
-              thumbnail: e.target.files[0],
-            });
-          }
-        }}
-      />
-      <input
-        type="text"
-        name="github"
-        placeholder="GitHub"
-        value={developer.github}
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="twitter"
-        placeholder="Twitter"
-        value={developer.twitter}
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="portfolioUrl"
-        placeholder="Portfolio URL"
-        value={developer.portfolioUrl}
-        onChange={handleChange}
-      />
-      <button type="submit">Submit</button>
+      {/* Your input fields here */}
     </form>
   );
 };
