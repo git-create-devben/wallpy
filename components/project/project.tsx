3697db93220e756e1e062e34ed6ed6a6ec3d8d9b
git-create-devben <DevBen.tech@outlook.com>
2023-11-03 10:20:08 +0100

    style

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -32,7 +32,7 @@ const Project: React.FC<ProjectProps> = ({ imageList, setLinkProp, githubLink, p
             <ul className="flex justify-between p-2">
               {portfolioLink ? (
                 <Link href={portfolioLink} className="flex flex-1">
-                  <BiLinkExternal />
+                  <BiLinkExternal className="text-2xl text-white"/>
                 </Link>
               ) : null}
               {socialLink ? (
