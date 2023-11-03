53e3cce76289ec7b005ad9ff5ddd4752b45d68f2
git-create-devben <DevBen.tech@outlook.com>
2023-11-03 20:33:38 +0100

    modify

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { supabase } from '@supabase/supabase-js';
 
-type Props = {}
-
-const UploadButton = (props: Props) => {
-  return (
-    <div>UploadButton</div>
-  )
+interface Developer {
+  name: string;
+  thumbnail: string;
+  github: string;
+  twitter: string;
+  portfolioUrl: string;
 }
 
-export default UploadButton
+const DeveloperForm: React.FC = () => {
+  const [developer, setDeveloper] = useState<Developer>({
+    name: '',
+    thumbnail: '',
+    github: '',
+    twitter: '',
+    portfolioUrl: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDeveloper({
+      ...developer,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // Upload the thumbnail image to Supabase Storage.
+    const storageRef = supabase.storage().from('thumbnails');
+    const thumbnailRef = storageRef.child(developer.name);
+    await thumbnailRef.upload(developer.thumbnail);
+
+    // Save the developer information to Supabase Database.
+    const databaseRef = supabase.from('developers');
+    await databaseRef.insert({
+      name: developer.name,
+      thumbnailUrl: await thumbnailRef.getUrl(),
+      github: developer.github,
+      twitter: developer.twitter,
+      portfolioUrl: developer.portfolioUrl,
+    });
+
+    // Clear the form.
+    setDeveloper({
+      name: '',
+      thumbnail: '',
+      github: '',
+      twitter: '',
+      portfolioUrl: '',
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={developer.name}
+        onChange={handleChange}
+      />
+      <input
+        type="file"
+        name="thumbnail"
+        placeholder="Thumbnail"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="github"
+        placeholder="GitHub"
+        value={developer.github}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="twitter"
+        placeholder="Twitter"
+        value={developer.twitter}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="portfolioUrl"
+        placeholder="Portfolio URL"
+        value={developer.portfolioUrl}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default DeveloperForm;
