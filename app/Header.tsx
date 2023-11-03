d526e40cb41e3a51f80e18a0f8edeb6fa5f9f210
git-create-devben <DevBen.tech@outlook.com>
2023-11-04 00:49:18 +0100

    update

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { UserButton } from "@clerk/nextjs";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { ModeToggle } from "@/components/ui/mode";
 import Uploadbutton from "@/components/UploadButton";
 import { Developer } from "../components/UploadButton";
@@ -9,31 +8,21 @@ interface HeaderProps {
   developerData: Developer;
 }
 
-const Header: React.FC<HeaderProps> = ({developerData}) => {
-
-
-
-
+const Header: React.FC<HeaderProps> = ({ developerData }) => {
   return (
-    <header className=" flex justify-between p-2">
-      <div className=" flex items-center text-center gap-6 w-full mb-2">
-        <h2 className=" text-4xl font-bold mb-2">
-          <span className=" text-blue-500">W</span>
-          <span className=" text-red-500">a</span>
-          <span className=" text-green-500">l</span>
-          <span className=" text-yellow-500">l</span>
-          <span className=" text-purple-500">p</span>
-          <span className=" text-pink-500">y</span>
-          <span className=" text-orange-500">.</span>
+    <header className="flex justify-between p-2">
+      <div className="flex items-center text-center gap-6 w-full mb-2">
+        <h2 className="text-4xl font-bold mb-2">
+          <span className="text-blue-500">W</span>
+          <span className="text-red-500">a</span>
+          <span className="text-green-500">l</span>
+          <span className="text-yellow-500">l</span>
+          <span className="text-purple-500">p</span>
+          <span className="text-pink-500">y</span>
+          <span className="text-orange-500">.</span>
         </h2>
-
-        {/* <input
-          type="search"
-          placeholder="Type here"
-          className=" input input-bordered w-full mt-2 max-w-[35rem] h-14 max-xl:max-w-[20rem] xl:block md:hidden sm:hidden"
-        /> */}
       </div>
-      <div className=" flex gap-4">
+      <div className="flex gap-4">
         <Uploadbutton developerData={developerData} />
         <ModeToggle />
         <UserButton afterSignOutUrl="/" />
