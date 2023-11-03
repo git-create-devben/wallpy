7837092fc3d2a03a7b7c9c13627392b2570d238e
git-create-devben <DevBen.tech@outlook.com>
2023-11-03 23:39:55 +0100

    update

--- app/firebase.ts
+++ app/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getStorage} from "firebase/storage"
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
