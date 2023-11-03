ae26928a55cf4edcf80aaa72338c78708b050a4f
git-create-devben <DevBen.tech@outlook.com>
2023-11-04 00:42:10 +0100

    update

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -10,10 +10,10 @@ const Project: React.FC<ProjectProps> = ({ developerData }) => {
   return (
     <div>
     <h2>Developer Information</h2>
-    <p>Name: {developerData.name}</p>
-    <p>Github: {developerData.github}</p>
-    <p>Twitter: {developerData.twitter}</p>
-    <p>Portfolio URL: {developerData.portfolioUrl}</p>
+    <p>Name: {developerData?.name || 'No Name Available'}</p>
+    <p>Github: {developerData?.github || 'No Github Available'}</p>
+    <p>Twitter: {developerData?.twitter || 'No Twitter Available'}</p>
+    <p>Portfolio URL: {developerData?.portfolioUrl || 'No Portfolio URL Available'}</p>
     {/* You can add more data points as needed */}
   </div>
   )
