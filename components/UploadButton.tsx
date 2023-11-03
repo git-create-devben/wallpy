c4fe5b5fc9bf4263b3cfccf4afe0b8db70dceced
git-create-devben <DevBen.tech@outlook.com>
2023-11-03 23:18:26 +0100

    update

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import 'firebase/storage';
-import {storage} from '../app/firebase'; // Update the path to your Firebase config file
+import { storage } from '../app/firebase'; // Update the path to your Firebase config file
 import { ref, uploadBytes } from 'firebase/storage';
 
 interface Developer {
@@ -32,65 +32,63 @@ const UploadButton: React.FC = () => {
 
     const storageRef = ref(storage, `thumbnails/${developer.name}`);
     if (developer.thumbnail) {
-      uploadBytes(storageRef, developer.thumbnail).then((snapshot: any) => {
+      uploadBytes(storageRef, developer.thumbnail).then((snapshot) => {
         console.log('Uploaded a blob or file!', snapshot);
         // Handle the rest of your logic here, e.g., saving the developer information to Firebase Firestore.
-      }).catch((error: any) => {
+      }).catch((error) => {
         console.error('Error uploading file:', error);
       });
     } else {
       console.error('No file chosen.');
     }
+  };
 
   return (
-    <>
-    {/* Your input fields here */}
     <form onSubmit={handleSubmit}>
-    <input
-      type="text"
-      name="name"
-      placeholder="Name"
-      value={developer.name}
-      onChange={handleChange}
-    />
-    <input
-      type="file"
-      name="thumbnail"
-      placeholder="Thumbnail"
-      onChange={(e) => {
-        if (e.target.files) {
-          setDeveloper({
-            ...developer,
-            thumbnail: e.target.files[0],
-          });
-        }
-      }}
-    />
-    <input
-      type="text"
-      name="github"
-      placeholder="GitHub"
-      value={developer.github}
-      onChange={handleChange}
-    />
-    <input
-      type="text"
-      name="twitter"
-      placeholder="Twitter"
-      value={developer.twitter}
-      onChange={handleChange}
-    />
-    <input
-      type="text"
-      name="portfolioUrl"
-      placeholder="Portfolio URL"
-      value={developer.portfolioUrl}
-      onChange={handleChange}
-    />
-    <button type="submit">Submit</button>
-  </form>
-  </>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={developer.name}
+        onChange={handleChange}
+      />
+      <input
+        type="file"
+        name="thumbnail"
+        placeholder="Thumbnail"
+        onChange={(e) => {
+          if (e.target.files) {
+            setDeveloper({
+              ...developer,
+              thumbnail: e.target.files[0],
+            });
+          }
+        }}
+      />
+      <input
+        type="text"
+        name="github"
+        placeholder="GitHub"
+        value={developer.github}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="twitter"
+        placeholder="Twitter"
+        value={developer.twitter}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="portfolioUrl"
+        placeholder="Portfolio URL"
+        value={developer.portfolioUrl}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
-}
+
 export default UploadButton;
