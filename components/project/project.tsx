37b6d2f10464403c0d188612284206eb774dc1de
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 00:53:55 +0100

    update

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,21 +1,68 @@
-import React from 'react';
-import { Developer } from '../UploadButton';
+import React, { useState, useEffect } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
+import { Developer } from './UploadButton';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import Image from 'next/image';
 
-type ProjectProps = {
-  developerData: Developer;
-};
+const DisplayComponent: React.FC = () => {
+  const [developers, setDevelopers] = useState<Developer[]>([]);
 
-const Project: React.FC<ProjectProps> = ({ developerData }) => {
+  useEffect(() => {
+    const fetchDevelopers = async () => {
+      const firestore = getFirestore();
+      const developersCollectionRef = collection(firestore, 'developers');
+      const querySnapshot = await getDocs(developersCollectionRef);
+      const fetchedDevelopers: Developer[] = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data() as Developer;
+        fetchedDevelopers.push(data);
+      });
+      setDevelopers(fetchedDevelopers);
+    };
+    fetchDevelopers();
+  }, []);
+
+  const getDeveloperThumbnail = async (path: string) => {
+    const storage = getStorage();
+    const storageRef = ref(storage, path);
+    const url = await getDownloadURL(storageRef);
+    return url;
+  };
+
+  
   return (
     <div>
-      <h2>Developer Information</h2>
-      <p>Name: {developerData?.name || 'No Name Available'}</p>
-      <p>Github: {developerData?.github || 'No Github Available'}</p>
-      <p>Twitter: {developerData?.twitter || 'No Twitter Available'}</p>
-      <p>Portfolio URL: {developerData?.portfolioUrl || 'No Portfolio URL Available'}</p>
-      {/* You can add more data points as needed */}
+      {developers.map((dev, index) => (
+        <div key={index}>
+          <h2>Developer Information</h2>
+          <p>Name: {dev.name}</p>
+          <p>Github: {dev.github}</p>
+          <p>Twitter: {dev.twitter}</p>
+          <p>Portfolio URL: {dev.portfolioUrl}</p>
+          {dev.thumbnail && (
+            <Image
+              width={200}
+              height={200}
+              className="rounded-lg"
+              layout="fixed"
+              objectFit="cover"
+              quality={100}
+              priority={true}
+              loading="eager"
+              blurDataURL={dev.thumbnail}
+              placeholder="blur"
+              // src={getDeveloperThumbnail(dev.thumbnail)}
+              src ={getDeveloperThumbnail(dev.thumbnail)}
+              alt="Developer Thumbnail"
+            />
+           
+            
+          )}
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Project;
+export default DisplayComponent;
