f3ffbc2a242f257b2b04a747b2b6f75b04ea4db9
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 15:47:29 +0100

    fixed bug

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '@/app/firebase';
+import Image from 'next/image';
 
-type Props = {}
+type DeveloperData = {
+  id: string;
+  textVal: string;
+  thumnailsUrl: string;
+}[];
 
-const project = (props: Props) => {
+const Project = (props: DeveloperData) => {
+  const [developerData, setDeveloperData] = useState<DeveloperData>([]);
+
+
+  const getData = async () => {
+    const project = collection(db, "developersInfo");
+    const datadb = await getDocs(project);
+    const allInfo = datadb.docs.map((val) => ({
+      id: val.id,
+      textVal: val.data().textVal,
+      thumnailsUrl: val.data().thumnailsUrl,
+    }));
+    setDeveloperData(allInfo);
+  };
+  
+  
+  useEffect(() => {
+    getData()
+  },[])
+
   return (
-    <div>project</div>
-  )
-}
+    <div>
+      {developerData.map((value) => (
+        <div key={value.id}>
+          <h1>{value.textVal}</h1>
+          <Image src={value.thumnailsUrl} height={200} width={200} alt={value.textVal} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default project
+export default Project;
