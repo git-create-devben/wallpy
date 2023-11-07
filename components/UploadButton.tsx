5a0fc1827e6045bed2283b6de5784f9da963c1d1
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 15:30:49 +0100

    update

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,18 +1,24 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { storage, db } from '@/app/firebase'
 import { v4 } from 'uuid'
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 import { addDoc, collection, getDocs } from 'firebase/firestore'
+import { Divide } from 'lucide-react'
+import Image from 'next/image'
 
 
-type Developer = {}
+type DeveloperData = {
+  id: string;
+  textVal: string;
+  thumnailsUrl: string;
+}[];
 
-const UploadButton = (props: Developer) => {
+const UploadButton = (props: DeveloperData) => {
 const [info, setInfo] = useState("")
 const [thumbnail, setThumbnail] = useState("")
-const [developerData, setDeveloperData] = useState([])]
+const [developerData, setDeveloperData] = useState<DeveloperData>([]);
 
 const uploadImage = (e: any) => {
   console.log(e.target.files[0])
@@ -32,16 +38,39 @@ await addDoc(projects, {textVal: info, thumnailsUrl:thumbnail })
 }
 
 const getData = async () => {
-  const project = collection(db, "developersInfo")
-  const datadb = await getDocs(project)
-  const allInfo = datadb.docs.map(val => ({...val.data(), id:val.id}))
-  
-}
+  const project = collection(db, "developersInfo");
+  const datadb = await getDocs(project);
+  const allInfo = datadb.docs.map((val) => ({
+    id: val.id,
+    textVal: val.data().textVal,
+    thumnailsUrl: val.data().thumnailsUrl,
+  }));
+  setDeveloperData(allInfo);
+};
+
+
+useEffect(() => {
+  getData()
+},[])
+
+console.log(developerData, "datadata")
   return (
     <div>
       <input onChange={(e) => setInfo(e.target.value)}/> <br/>
       <input type="file" onChange={(e) => uploadImage(e) }/>
       <button onClick={upload}>Upload</button>
+
+      {
+        developerData.map(value => 
+          
+          // eslint-disable-next-line react/jsx-key
+          <div>
+            <h1>{value.textVal}</h1>
+            <Image src={value.thumnailsUrl} height={200} width={200} alt={value.textVal}/>
+          </div>
+          
+          )
+      }
     </div>
   )
 }
