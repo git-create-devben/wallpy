423c6ef4bc817850170372d97277c807c1ed67b5
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 01:13:09 +0100

    refactor the code to avoid async/ await

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React, { useState, useEffect } from 'react';
 import { getDocs, collection } from 'firebase/firestore';
 import { getFirestore } from 'firebase/firestore';
@@ -7,48 +5,57 @@ import { Developer } from '../UploadButton';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import Image from 'next/image';
 
-const Project: React.FC = () => {
+const DisplayComponent: React.FC = () => {
   const [developers, setDevelopers] = useState<Developer[]>([]);
 
   useEffect(() => {
-    const fetchDevelopers = async () => {
+    const fetchDevelopers = () => {
       const firestore = getFirestore();
       const developersCollectionRef = collection(firestore, 'developers');
-      const querySnapshot = await getDocs(developersCollectionRef);
-      const fetchedDevelopers: Developer[] = [];
-      querySnapshot.forEach((doc) => {
-        const data = doc.data() as Developer;
-        fetchedDevelopers.push(data);
-      });
-      setDevelopers(fetchedDevelopers);
+      getDocs(developersCollectionRef)
+        .then((querySnapshot) => {
+          const fetchedDevelopers: Developer[] = [];
+          querySnapshot.forEach((doc) => {
+            const data = doc.data() as Developer;
+            fetchedDevelopers.push(data);
+          });
+          setDevelopers(fetchedDevelopers);
+        })
+        .catch((error) => {
+          console.error('Error fetching developers: ', error);
+        });
     };
     fetchDevelopers();
   }, []);
 
-  const getDeveloperThumbnail = async (path: string) => {
+  const getDeveloperThumbnail = (path: string) => {
     const storage = getStorage();
     const storageRef = ref(storage, path);
-    const url = await getDownloadURL(storageRef);
-    return url;
+    return getDownloadURL(storageRef);
   };
 
   return (
     <div>
-      {developers.map(async (dev, index) => {
+      {developers.map((dev, index) => {
         if (dev.thumbnail) {
-          const thumbnailUrl = await getDeveloperThumbnail(dev.thumbnail.name);
-          return (
-            <div key={index}>
-              <h2>Developer Information</h2>
-              <p>Name: {dev.name}</p>
-              <p>Github: {dev.github}</p>
-              <p>Twitter: {dev.twitter}</p>
-              <p>Portfolio URL: {dev.portfolioUrl}</p>
-              {thumbnailUrl && (
-                <Image src={thumbnailUrl} alt="Developer Thumbnail" />
-              )}
-            </div>
-          );
+          getDeveloperThumbnail(dev.thumbnail.name)
+            .then((thumbnailUrl) => {
+              return (
+                <div key={index}>
+                  <h2>Developer Information</h2>
+                  <p>Name: {dev.name}</p>
+                  <p>Github: {dev.github}</p>
+                  <p>Twitter: {dev.twitter}</p>
+                  <p>Portfolio URL: {dev.portfolioUrl}</p>
+                  {thumbnailUrl && (
+                    <Image src={thumbnailUrl} alt="Developer Thumbnail" />
+                  )}
+                </div>
+              );
+            })
+            .catch((error) => {
+              console.error('Error fetching thumbnail URL: ', error);
+            });
         }
         return null;
       })}
@@ -56,4 +63,4 @@ const Project: React.FC = () => {
   );
 };
 
-export default Project;
+export default DisplayComponent;
