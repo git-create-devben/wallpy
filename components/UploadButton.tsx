eb4cfefb036e2bf0a85ece0d9d8ff1651db08fd7
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 14:38:52 +0100

    update

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,139 +1,31 @@
-import React, { useState } from "react";
-import { storage } from "../app/firebase";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import React, { useState } from 'react'
+import { storage, db } from '@/app/firebase'
+import { v4 } from 'uuid'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 
-export interface Developer {
-  name: string;
-  thumbnail: string | null; // Updated the type to 'string'
-  github: string;
-  twitter: string;
-  portfolioUrl: string;
+
+type Developer = {}
+
+const UploadButton = (props: Developer) => {
+const [info, setInfo] = useState("")
+const [thumbnail, setThumbnail] = useState("")
+
+const uploadImage = (e: any) => {
+  console.log(e.target.files[0])
+  const thumbnails = ref(storage, `thumbnail/s${v4()}`)
+  uploadBytes(thumbnails, e.target.files[0]).then(data => {
+    console.log(data, "thumbnails")
+    getDownloadURL(data.ref).then(val => {
+      console.log(val)
+    })
+  })
 }
-
-interface UploadButtonProps {
-  developerData?: Developer;
-}
-
-const UploadButton: React.FC<UploadButtonProps> = ({ developerData }) => {
-  const [developer, setDeveloper] = useState<Developer>({
-    name: "",
-    thumbnail: null,
-    github: "",
-    twitter: "",
-    portfolioUrl: "",
-  });
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDeveloper({
-      ...developer,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-  
-    if (developer.thumbnail) {
-      const storage = getStorage();
-      const storageRef = ref(storage, `thumbnails/${developer.name}`); // Ensure the appropriate path to the image in Firebase Storage
-  
-      const file = developer.thumbnail;
-      const reader = new FileReader();
-      reader.readAsArrayBuffer(file);
-  
-      reader.onload = function (event) {
-        if (event.target && event.target.result) {
-          const fileData = event.target.result as ArrayBuffer;
-          const blob = new Blob([new Uint8Array(fileData)]);
-          uploadBytes(storageRef, blob) // Pass the created blob
-            .then(async (snapshot) => {
-              console.log("Uploaded a blob or file!", snapshot);
-  
-              const storageBucketUrl =
-                "https://firebasestorage.googleapis.com/v0/b/wallpy5.appspot.com/o/thumbnails"; // Replace with your storage bucket URL
-              const fileUrl = `${storageBucketUrl}/${developer.thumbnail}`;
-              console.log("File URL:", fileUrl);
-  
-              const firestore = getFirestore();
-              const developersCollectionRef = collection(firestore, "developers");
-  
-              await addDoc(developersCollectionRef, {
-                name: developer.name,
-                thumbnailUrl: snapshot.metadata.fullPath,
-                github: developer.github,
-                twitter: developer.twitter,
-                portfolioUrl: developer.portfolioUrl,
-              });
-  
-              // Handle the rest of your logic here, if necessary.
-            })
-            .catch((error: any) => {
-              console.error("Error uploading file:", error);
-            });
-        }
-      };
-  
-      reader.onerror = function (event) {
-        console.error("Error reading the file:", event);
-      };
-    } else {
-      console.error("No file chosen.");
-    }
-  };
-  
-  
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={developer.name}
-          onChange={handleChange}
-        />
-        <input
-          type="file"
-          name="thumbnail"
-          placeholder="Thumbnail"
-          onChange={(e) => {
-            if (e.target.files) {
-              const file = e.target.files[0];
-              if (file) {
-                setDeveloper({
-                  ...developer,
-                  thumbnail: file.name, // Assuming 'name' is the path to the image in Firebase Storage
-                });
-              }
-            }
-          }}
-        />
-        <input
-          type="text"
-          name="github"
-          placeholder="GitHub"
-          value={developer.github}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="twitter"
-          placeholder="Twitter"
-          value={developer.twitter}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="portfolioUrl"
-          placeholder="Portfolio URL"
-          value={developer.portfolioUrl}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <input onChange={(e) => setInfo(e.target.value)}/>
+      <input type="file" onChange={(e) => uploadImage(e) }/>
     </div>
-  );
-};
+  )
+}
 
-export default UploadButton;
+export default UploadButton
--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,74 +1,11 @@
-"use client"
+import React from 'react'
 
-import React, { useState, useEffect } from 'react';
-import { getDocs, collection } from 'firebase/firestore';
-import { getFirestore } from 'firebase/firestore';
-import { Developer } from '../UploadButton';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
-import Image from 'next/image';
+type Props = {}
 
-const Project: React.FC = () => {
-  const [developers, setDevelopers] = useState<Developer[]>([]);
-  const [thumbnailUrls, setThumbnailUrls] = useState<string[]>([]);
-
-  useEffect(() => {
-    const fetchDevelopers = async () => {
-      const firestore = getFirestore();
-      const developersCollectionRef = collection(firestore, 'developers');
-      const querySnapshot = await getDocs(developersCollectionRef);
-      const fetchedDevelopers: Developer[] = [];
-      querySnapshot.forEach((doc) => {
-        const data = doc.data() as Developer;
-        fetchedDevelopers.push(data);
-      });
-      setDevelopers(fetchedDevelopers);
-    };
-    fetchDevelopers();
-  }, []);
-
-  useEffect(() => {
-    const fetchThumbnailUrls = async () => {
-      const storage = getStorage();
-      const urls = await Promise.all(
-        developers.map(async (dev) => {
-          if (dev.thumbnail) {
-            const storageRef = ref(storage, dev.thumbnail); // Assuming dev.thumbnail is the path to the image in Firebase Storage
-            const url = await getDownloadURL(storageRef);
-            return url;
-          }
-          return ''; // Return a default value if dev.thumbnail doesn't exist
-        })
-      );
-      setThumbnailUrls(urls);
-    };
-    fetchThumbnailUrls();
-  }, [developers]);
-
+const project = (props: Props) => {
   return (
-    <div>
-      {developers.map((dev, index) => (
-        <div key={index}>
-          <h2>Developer Information</h2>
-          {dev.thumbnail && thumbnailUrls[index] && (
-            <div>
-              <h2>image</h2>
-              <Image
-                src={thumbnailUrls[index]}
-                alt={dev.name}
-                width={100}
-                height={100}
-                objectFit="cover"
-              />
-            </div>
-          )}
-          <p>Name: {dev.name}</p>
-          <p>Github: {dev.github}</p>
-          <p>Twitter: {dev.twitter}</p>
-          <p>Portfolio URL: {dev.portfolioUrl}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+    <div>project</div>
+  )
+}
 
-export default Project;
+export default project
