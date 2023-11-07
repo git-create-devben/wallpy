a221d8f6996da2fa373e606f37e0f53f216807bd
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 18:05:24 +0100

    update

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -77,7 +77,7 @@ const UploadButton = (props: DeveloperData) => {
                 d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
               />
             </svg>
-            <span>Warning: After upload you wont be able to edit so becarefull when filling the forms or you can contact devvben for an error!</span>
+            <span>Warning: After upload you wont be able to edit so becarefull when filling the forms!</span>
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
--- components/project/project.tsx
+++ components/project/project.tsx
@@ -47,17 +47,42 @@ const Project = (props: DeveloperData) => {
             width={400}
             alt={value.textVal}
           />
-          <div key={value.id} className="child-div flex justify-around items-center p-4">
+          <div
+            key={value.id}
+            className="child-div flex justify-around items-center p-4"
+          >
             <h1>{value.textVal}</h1>
             <Link href={value.github} target="_blank">
-              <FaGithub className="w-10 h-10"/>
+              <FaGithub className="w-10 h-10" />
             </Link>
             <Link href={value.portfolio} target="_blank">
-              <FaExternalLinkAlt className="w-10 h-10"/>
+              <FaExternalLinkAlt className="w-10 h-10" />
             </Link>
           </div>
         </div>
       ))}
+
+      <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600"></div>
+        <div className="p-6">
+          <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            Tailwind card
+          </h5>
+          <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis
+            ligula.
+          </p>
+        </div>
+        <div className="p-6 pt-0">
+          <button
+            data-ripple-light="true"
+            type="button"
+            className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          >
+            Read More
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
