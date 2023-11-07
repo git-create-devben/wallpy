8a075196a7d042f7f667cefb72ecc83d97de8358
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 17:54:46 +0100

    add warning

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -63,7 +63,22 @@ const UploadButton = (props: DeveloperData) => {
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Add portfolio</DialogTitle>
-          <DialogDescription></DialogDescription>
+          <DialogDescription className="alert alert-warning">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <span>Warning: After upload you wont be able to edit so becarefull when filling the forms or you can contact devvben for an error!</span>
+          </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
