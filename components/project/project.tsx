83d7707d265ae095250340a0ec7d30a21e8fa072
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 18:12:15 +0100

    update

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -72,7 +72,7 @@ const Project = (props: DeveloperData) => {
         </div>
         <div className="p-6">
           <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-            Tailwind card
+            {value.textVal}
           </h5>
           <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis
