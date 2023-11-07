61723f314bd1ba3ec8b91ef207786e4c69336186
git-create-devben <DevBen.tech@outlook.com>
2023-11-07 17:01:57 +0100

    styling

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '@/app/firebase';
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/app/firebase";
+import Image from "next/image";
 
 type DeveloperData = {
   id: string;
@@ -15,7 +15,6 @@ type DeveloperData = {
 
 const Project = (props: DeveloperData) => {
   const [developerData, setDeveloperData] = useState<DeveloperData>([]);
-
 
   const getData = async () => {
     const project = collection(db, "developersInfo");
@@ -30,21 +29,26 @@ const Project = (props: DeveloperData) => {
     }));
     setDeveloperData(allInfo);
   };
-  
-  
-  
+
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
-    <div>
+    <div className="flex gap-2">
       {developerData.map((value) => (
-        <div key={value.id} className='flex '>
-          <h1>{value.textVal}</h1>
-          <Image src={value.thumnailsUrl} height={200} width={200} alt={value.textVal} />
-          <h2>{value.github}</h2>
-          <h2>{value.portfolio}</h2>
+        <div key={value.id} className="">
+          <Image
+            src={value.thumnailsUrl}
+            height={200}
+            width={200}
+            alt={value.textVal}
+          />
+          <div className="flex">
+            <h1>{value.textVal}</h1>
+            <h2>{value.github}</h2>
+            <h2>{value.portfolio}</h2>
+          </div>
         </div>
       ))}
     </div>
