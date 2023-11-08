9fa5a1b54a09ef4aae0c1a84cffaea82a2231424
git-create-devben <DevBen.tech@outlook.com>
2023-11-09 00:56:45 +0100

    testing...

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -79,36 +79,43 @@ const Project = (props: DeveloperData) => {
         //     </Link>
         //   </div>
         // </div>
-        <div
-          key={value.id}
-          className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md"
-        >
-          <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
-            <Image
-              src={value.thumnailsUrl}
-              alt="Tailwind card"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <div className="p-6">
-            <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-              {value.textVal}
-            </h5>
-            <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-             Built with: {value.descriptionTest}
-            </p>
-          </div>
-          <div className="p-6 pt-0">
-            <button
-              data-ripple-light="true"
-              type="button"
-              className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            >
-              Read More
-            </button>
-          </div>
+        // <div
+        //   key={value.id}
+        //   className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md"
+        // >
+        //   <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+        //     <Image
+        //       src={value.thumnailsUrl}
+        //       alt="Tailwind card"
+        //       layout="fill"
+        //       objectFit="cover"
+        //     />
+        //   </div>
+        //   <div className="p-6">
+        //     <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+        //       {value.textVal}
+        //     </h5>
+        //     <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+        //      Built with: {value.descriptionTest}
+        //     </p>
+        //   </div>
+        //   <div className="p-6 pt-0">
+        //     <button
+        //       data-ripple-light="true"
+        //       type="button"
+        //       className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        //     >
+        //       Read More
+        //     </button>
+        //   </div>
+        // </div>
+
+        <div className="book"  key={value.id}>
+        <p>Hello</p>
+        <div className="cover">
+            <p>Hover Me</p>
         </div>
+       </div>
       ))}
     </div>
   );
