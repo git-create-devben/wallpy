fb2896d96289b2843a5f8ccdd278ec0adf9b05f6
git-create-devben <DevBen.tech@outlook.com>
2023-11-09 00:36:24 +0100

    update

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -80,9 +80,9 @@ const UploadButton = (props: DeveloperData) => {
                 d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
               />
             </svg>
-            <span>
-              Warning: After upload you wont be able to edit so becarefull when
-              filling the forms!
+            <span className="text-white">
+              Warning: After You upload your portfolio wont be able to edit so becarefull when
+              filling the form!
             </span>
           </DialogDescription>
         </DialogHeader>
