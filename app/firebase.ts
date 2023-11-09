8b96ac937978e3df1b8c621520c8e243127d33f4
git-create-devben <DevBen.tech@outlook.com>
2023-11-09 10:19:41 +0100

    remove analytics

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import {getStorage} from "firebase/storage"
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,7 +20,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const storage = getStorage(app);
 const db = getFirestore();
 
