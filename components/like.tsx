90a200dcc135ad8450f278d11ddada70c7ace40c
git-create-devben <DevBen.tech@outlook.com>
2023-11-10 17:01:59 +0100

    add click vote setup

---
+++ components/like.tsx
@@ -0,0 +1,21 @@
+import {
+    ClickVoteComponent,
+    ClickVoteProvider,
+    LikeStyle,
+  } from '@clickvote/react';
+  
+  function RenderComponent () {
+      return (
+        <ClickVoteProvider
+          value={{
+            apiUrl: "socket.clickvote.dev",
+            publicKey: "pb_QFHQfHcuQxOvLJNaOjjOeFo9wOrZbTN7",
+            userId: "USER_ID"
+          }}
+        >
+          <ClickVoteComponent id="wallpy" voteTo="VOTE_TO">
+            {(props) => <LikeStyle {...props} />}
+          </ClickVoteComponent>
+        </ClickVoteProvider>
+      );
+  };
