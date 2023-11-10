85d8071abb3248c9377e32b4c8b1cc9a75f32e59
git-create-devben <DevBen.tech@outlook.com>
2023-11-10 17:34:06 +0100

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 // import Image from "next/image";
 
 import Hero from "./Hero";
-
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div>
            <Hero/>
+           <Footer/>
     </div>
   );
 }
