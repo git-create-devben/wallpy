555632e7ae798111225e421f313ec1cebfda74af
git-create-devben <DevBen.tech@outlook.com>
2023-11-10 17:23:52 +0100

    ?

--- components/like.tsx
+++ components/like.tsx
@@ -1,25 +1,25 @@
 import {
-    ClickVoteComponent,
-    ClickVoteProvider,
-    LikeStyle,
-  } from '@clickvote/react';
-  
-  function Like () {
-      return (
-        <div>
-                <ClickVoteProvider
-          value={{
-            apiUrl: "socket.clickvote.dev",
-            publicKey: "pb_QFHQfHcuQxOvLJNaOjjOeFo9wOrZbTN7",
-            userId: "visitor"
-          }}
-        >
-          <ClickVoteComponent id="wallpy" voteTo="VOTE_TO">
-            {(props) => <LikeStyle {...props} />}
-          </ClickVoteComponent>
-        </ClickVoteProvider>
-        </div>
-      )
-  };
+  ClickVoteComponent,
+  ClickVoteProvider,
+  LikeStyle,
+} from "@clickvote/react";
 
-  export default Like;
+function Like() {
+  return (
+    <div className="w-[200px] bg-white">
+      <ClickVoteProvider
+        value={{
+          apiUrl: "socket.clickvote.dev",
+          publicKey: "pb_QFHQfHcuQxOvLJNaOjjOeFo9wOrZbTN7",
+          userId: "visitor",
+        }}
+      >
+        <ClickVoteComponent id="wallpy" voteTo="VOTE_TO">
+          {(props) => <LikeStyle {...props} />}
+        </ClickVoteComponent>
+      </ClickVoteProvider>
+    </div>
+  );
+}
+
+export default Like;
