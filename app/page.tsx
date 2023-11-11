d58a4af741ff695ca2e49dbce0f12ba594daeaef
git-create-devben <DevBen.tech@outlook.com>
2023-11-11 16:05:46 +0100

    disable nossr

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,17 @@
 import Hero from "./Hero";
 import Footer from "@/components/footer";
 
+import dynamic from 'next/dynamic';
+
+const Header = dynamic(() => import('./Header'), {
+  ssr: false,
+});
+
+
 export default function Home() {
   return (
     <div>
+      <Header/>
            <Hero/>
            <Footer/>
     </div>
