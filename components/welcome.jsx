1e2c4cb67e1981ea99d5b77dd92887e65c86c108
git-create-devben <DevBen.tech@outlook.com>
2023-11-11 16:59:19 +0100

    update

--- components/welcome.jsx
+++ components/welcome.jsx
@@ -4,15 +4,14 @@ import React from 'react';
 
 const Modal = ({ onClose }) => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black">
-      {/* Background overlay with blur */}
-      <div className="fixed top-0 left-0 w-full h-full bg-gray-800 opacity-50"></div>
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-primary ">
 
       {/* Modal content */}
       <div className="bg-white rounded-lg p-8 z-50">
-        <p className="text-2xl font-bold mb-4">Welcome to Our Project!</p>
+        <p className="text-2xl font-bold mb-4">Welcome to Wallpy</p>
         <p className="text-gray-700">
           This project is still under development, and we welcome contributors.
+
         </p>
 
         {/* Close button */}
