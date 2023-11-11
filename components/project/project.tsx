b8a49bd7d96bb39caf555f3fc48b1074aaa683cc
git-create-devben <DevBen.tech@outlook.com>
2023-11-11 16:13:37 +0100

    update

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -56,24 +56,30 @@ const Project = (props: DeveloperData) => {
 
   if (loading) {
     return (
-      <><div className="overlay" id="overlay"></div><div className="loaded">
-        <div className="loader-cube">
-          <div className="face"></div>
-          <div className="face"></div>
-          <div className="face"></div>
-          <div className="face"></div>
-          <div className="face"></div>
-          <div className="face"></div>
+      <>
+        <div className="overlay" id="overlay"></div>
+        <div className="loaded">
+          <div className="loader-cube">
+            <div className="face"></div>
+            <div className="face"></div>
+            <div className="face"></div>
+            <div className="face"></div>
+            <div className="face"></div>
+            <div className="face"></div>
+          </div>
+          <p className="text-white text-sm mt-8">loading....</p>
         </div>
-        <p className="text-white text-sm mt-8">loading....</p>
-      </div></>
+      </>
     );
   }
 
   return (
     <div>
       {developerData.map((value) => (
-        <div className="  card card-compact w-96 bg-base-100 shadow-xl" key={value.id}>
+        <div
+          className="  card card-compact w-96 bg-base-100 shadow-xl"
+          key={value.id}
+        >
           <button className="mail"></button>
           <div className="thumbnail">
             <Image
@@ -87,37 +93,36 @@ const Project = (props: DeveloperData) => {
               quality={100}
             />
           </div>
-            <div className=" card-body">
-              <h1 className="card-title">{value.textVal}</h1>
-              <p className="about-me text-white text-xl text-bold ">
-                Build with: {value.descriptionTest}
-              </p>
+          <div className=" card-body">
+            <h1 className="card-title">{value.textVal}</h1>
+            <p className="about-me text-white text-xl text-bold ">
+              Build with: {value.descriptionTest}
+            </p>
+          </div>
+          <div className="bottom-bottom flex items-center justify-between">
+            <div className="social-links-container flex gap-2 items-center">
+              {value.github ? (
+                <Link href={value.github}>
+                  <FaGithub className="h-8 w-8 fa" />
+                </Link>
+              ) : null}
+              {value.twitter ? (
+                <Link href={value.twitter}>
+                  <FaXTwitter className="h-8 w-8 fa" />
+                </Link>
+              ) : null}
+              {value.thread ? (
+                <Link href={value.thread}>
+                  <FaThreads className="h-8 w-8 fa" />
+                </Link>
+              ) : null}
             </div>
-            <div className="bottom-bottom flex items-center justify-between">
-              <div className="social-links-container flex gap-2 items-center">
-                {value.github ? (
-                  <Link href={value.github}>
-                    <FaGithub className="h-8 w-8 fa" />
-                  </Link>
-                ) : null}
-                {value.twitter ? (
-                  <Link href={value.twitter}>
-                    <FaXTwitter className="h-8 w-8 fa" />
-                  </Link>
-                ) : null}
-                {value.thread ? (
-                  <Link href={value.thread}>
-                    <FaThreads className="h-8 w-8 fa" />
-                  </Link>
-                ) : null}
-              </div>
-              {/* <Like/> */}
-              <Link href={value.portfolio}>
+            {/* <Like/> */}
+            <Link href={value.portfolio}>
               <button className="btn card-actions justify-end">
                 <FaExternalLinkAlt /> view
               </button>
-              </Link>
-           
+            </Link>
           </div>
         </div>
       ))}
