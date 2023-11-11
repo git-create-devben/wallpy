b4b265120c86d33779bb97bb96c490037700ea38
git-create-devben <DevBen.tech@outlook.com>
2023-11-11 16:08:24 +0100

    update

--- app/Header.tsx
+++ app/Header.tsx
@@ -45,7 +45,7 @@ export function Example() {
       </SignInButton> */}
     <SignedOut>
       {/* Signed out users get sign in button */}
-      <SignInButton redirectUrl="/app">
+      <SignInButton>
       <Button variant="outline">Sign in with Clerk</Button>
       </SignInButton>
     </SignedOut>
