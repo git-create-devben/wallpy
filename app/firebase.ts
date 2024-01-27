d1060ebbcd053967e632944914b60ad0b90ca7c2
git-create-devben <benlad636@gmail.com>
2024-01-27 05:44:56 -0800

    add recaptcha

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp} from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // import { getAnalytics } from "firebase/analytics";
 import {getStorage} from "firebase/storage"
 import { getFirestore } from "firebase/firestore";
@@ -24,5 +25,14 @@ const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const db = getFirestore();
 
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LeLNF4pAAAAAMyT8PzZW-JziVL8X_N85xq3y53T'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
 
 export { storage, db};
